5c224ff2018cea0ba9cc3111aa1b3c8b8f3a6d2f
tvraghavan <tvraghavan@gmail.com>
2021-07-26 08:13:37 +0530

    baseline

---
+++ src/main/java/com/example/demo/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("com.example")
+@EnableAdminServer
+public class AdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminServerApplication.class, args);
+	}
+
+}
